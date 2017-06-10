0ee4a42dd31aeacb3e1e835327d966d56dc45932
Alexey Milyukov <milyukov.alexey@gmail.com>
2017-06-10 01:40:22 -0700

    More monkey stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import ExploreWindow from './components/ExploreWindow';
+import Oups from './components/Oups';
 import AccountSettingsWindow from './components/AccountSettingsWindow';
 import logo from './logo.svg';
 import './App.css';
@@ -10,7 +11,21 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      main: <ExploreWindow />
+      main: 'explore'
+    }
+  }
+
+  getMainWindow(){
+    console.log(this.state.main);
+    switch(this.state.main){
+      case 'explore':
+        return <ExploreWindow />
+        break;
+      case 'account_settings':
+        return <AccountSettingsWindow />
+        break;
+      default:
+        return <Oups />
     }
   }
 
@@ -18,7 +33,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-        {this.state.main}
+        {this.getMainWindow()}
       </div>
     );
   }
---
+++ src/components/Oups.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Oups extends Component{
+
+  render(){
+    return(
+      <div>
+        Oups, something is wrong in the router.
+      </div>
+    )
+  }
+}
+
+export default Oups;
